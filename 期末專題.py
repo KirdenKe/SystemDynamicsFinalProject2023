be89c72da92686d28775a4b91c4bd07276867b42
LAPTOP-L53CFE5A\KirdenKe <kedengxian@gmail.com>
2023-05-18 22:59:27 +0800

    Complete the first question

---
+++ 期末專題.py
@@ -0,0 +1,57 @@
+import numpy as np
+import matplotlib.pylab as plt
+from scipy.integrate import odeint
+from vpython import *
+
+Posmag = 2.5
+Negmag = -2.5
+amplitude = Posmag - Negmag
+ncycle = 5
+period = 10
+totaltime = ncycle * period
+
+dt = 0.001
+npoint = totaltime / dt
+
+def ODE(x, t, m1, m2, b1, b2, k1, k2, F):
+    # 方波
+    a = t / period
+    aint = int(a)
+    residual = a - aint
+    if (residual <= 0.5):
+        F = Posmag
+        dx = x[1]; dx2 = x[2]; dx3 = x[3]
+        dx4 = (k2 * F - (m2 * (b1 + b2) + m1 * b2) * x[3] - (m2 * (k1 + k2) + b1 * b2 + m1 * k2) * x[2] - (b1 * k2 + b2 * k1) * x[1] - k1 * k2 * x[0]) / (m1 * m2)
+        return [dx, dx2, dx3, dx4]
+    else:
+        F = Negmag
+        dx = x[1]; dx2 = x[2]; dx3 = x[3]
+        dx4 = (k2 * F - (m2 * (b1 + b2) + m1 * b2) * x[3] - (m2 * (k1 + k2) + b1 * b2 + m1 * k2) * x[2] - (b1 * k2 + b2 * k1) * x[1] - k1 * k2 * x[0]) / (m1 * m2)
+        return [dx, dx2, dx3, dx4]
+
+t = np.arange(0, totaltime + dt, dt)
+square = np.zeros(t.size)
+time = 0
+for i in range(0, t.size):
+    a = time / period
+    a_int = int(a)
+    residual = a - a_int
+    if (residual <= 0.5):
+        square[i] = Posmag
+    else:
+        square[i] = Negmag
+    time += dt
+
+m1 = 1
+m2 = 1
+b1 = 1
+b2 = 1
+k1 = 1
+k2 = 1
+
+x_initial = [0, 0, 0, 0]
+x = odeint(ODE, x_initial, t, args = (m1, m2, b1, b2, k1, k2, square))
+plt.plot(t, square)
+plt.plot(t, x[:, 0])
+plt.grid()
+plt.show()
